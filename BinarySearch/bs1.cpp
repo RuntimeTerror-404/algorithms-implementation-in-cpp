9860a4276c5966bf272fc75ff6ce653aa317ef77
Mohit Parashar <mohit1672002@gmail.com>
2021-02-13 23:13:04 +0530

    first commit

--- BinarySearch/bs1.cpp
+++ BinarySearch/bs1.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
 using namespace std;
 
+void insertionSort(int a[], int n){
+
+    for(int i=1; i<n; i++){
+        int current = a[i];
+        int j = i-1;
+        while(a[j] > current && j>=0){
+            a[j+1] = a[j];
+            j--;
+        }
+        a[j+1] = current;
+    }
+}
+
 int BinarySearch(int a[], int target, int n){
     // int n = sizeof(a)/sizeof(int);
     int left = 0;
@@ -21,8 +34,14 @@ int BinarySearch(int a[], int target, int n){
 };
 
 int main(){
-    int a[] = {-2, 3, 4, 7, 8, 9, 11, 13};
+    // int a[] = {-2, 3, 4, 7, 8, 9, 11, 13};
+    int a[] = {21, 16, 9, 44, 23, 11, 8, 19, 13, 0, 33};
     int n = sizeof(a)/sizeof(int);
-    int x = BinarySearch(a, 10, n);
+    insertionSort(a,n);
+    for(int i=0; i<n; i++){
+        cout<<a[i]<<" , ";
+    }
+    cout<<" \n "<<endl;
+    int x = BinarySearch(a, 13, n);
     cout<<x;
 }
